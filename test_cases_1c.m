7a33ee07fe9e50d01365757971e85aed1bbf1ecf
Nene Matsuki <80789350+NeneMatsuki@users.noreply.github.com>
2022-04-30 14:49:48 +1200

    Update test_cases_1c.m

--- test_cases_1c.m
+++ test_cases_1c.m
@@ -108,19 +108,39 @@ classdef test_cases_1c < matlab.unittest.TestCase
         end
 
         function Assignment_1_Q4_redundant_constraint(testCase)
-            % I don't pass this :(
             m = 3;
             n = 5;
             
             A = [1 2 3 1 -1;
                  2 1 4 -1 2;
-                 -2 -1 -4 1 -2];
+                 2 1 4 -1 2];
             
             c = [1 1 1 1 1]';
             
-            b = [4 6 -6]';
+            b = [4 6 6]';
 
-            [z,x,pi,ind,exitflag] = rsm(A,b,c,m,n);
+                [z,x,pi,ind,exitflag] = rsm(A,b,c,m,n);
+
+            testCase.verifyEqual(z,8/5, RelTol = 1e-5);
+%             testCase.verifyEqual(x,[7/5,1/5].', RelTol = 1e-5);
+%             testCase.verifyEqual(pi,[-1/5,2/5].', RelTol = 1e-5);
+%             testCase.verifyEqual(ind,[3,5].', RelTol = 1e-5);
+            testCase.verifyEqual(exitflag,0, RelTol = 1e-5);
+        end
+
+        function Assignment_1_Q4_redundant_constraint_2(testCase)
+            m = 3;
+            n = 5;
+            
+            A = [1 2 3 1 -1;
+                 2 1 4 -1 2;
+                 1 2 1 1 -2];
+            
+            c = [1 1 1 1 1]';
+            
+            b = [4 6 1]';
+
+                [z,x,pi,ind,exitflag] = rsm(A,b,c,m,n);
 
             testCase.verifyEqual(z,8/5, RelTol = 1e-5);
 %             testCase.verifyEqual(x,[7/5,1/5].', RelTol = 1e-5);
@@ -172,6 +192,52 @@ classdef test_cases_1c < matlab.unittest.TestCase
              testCase.verifyEqual(exitflag,0, RelTol = 1e-5);
          end
 
+         function Tutorial_2_Q1(testCase)
+             % problem from tut
+             m = 4;
+             n = 6;
+ 
+             A = [2 4 1 0 0 0;
+                 4 4 0 1 0 0;
+                 6 3 0 0 1 0;
+                 -4 3 0 0 0 1];
+ 
+             b = [16 20 24 6].';
+ 
+             c = [-3 -2 0 0 0 0].';
+ 
+             [z,x,pi,ind,exitflag] = rsm(A,b,c,m,n);
+ 
+             testCase.verifyEqual(z,-13, RelTol = 1e-5);
+             testCase.verifyEqual(x,[3,12,2,2].', RelTol = 1e-5);
+             testCase.verifyEqual(pi,[0,-0.25,-1/3,0].', RelTol = 1e-5);
+             testCase.verifyEqual(ind,[1,6,3,2].', RelTol = 1e-5);
+             testCase.verifyEqual(exitflag,0, RelTol = 1e-5);
+         end
+
+
+        function Tutorial_2_Q2(testCase)
+             % problem from tut
+             m = 3;
+             n = 6;
+ 
+             A = [1 1 1 1 0 0;
+                 0 1 0 -1 1 0;
+                 1 0 -1 0 -1 1];
+ 
+             b = [4 4 4].';
+ 
+             c = [15 10 -10 1 2 3].';
+ 
+             [z,x,pi,ind,exitflag] = rsm(A,b,c,m,n);
+ 
+             testCase.verifyEqual(z,4, RelTol = 1e-5);
+             testCase.verifyEqual(x,[4,4,12].', RelTol = 1e-5);
+             testCase.verifyEqual(pi,[-7,5,3].', RelTol = 1e-5);
+             testCase.verifyEqual(ind,[3,5,6].', RelTol = 1e-5);
+             testCase.verifyEqual(exitflag,0, RelTol = 1e-5);
+         end
+
          function Assignment_1_Q5_Primal(testCase)
              % Primal problem from assignment adjusted to have equality
              % constraints.
@@ -183,6 +249,9 @@ classdef test_cases_1c < matlab.unittest.TestCase
              %
              % Supposed optimal x values in order:
              % [-0.2 0.4 0.4 1 0 1.6 0]
+
+             % Not passing this (Nikhil + Nene)
+
             m = 5;
             n = 7;
 
